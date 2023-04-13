a7f8119f760de9d30c1b1d9444c635d3599249c1
DarrenChangJR <darrenchangjr01@gmail.com>
2023-04-13 16:54:18 +0000

    Use only solc-select (pip)

--- code/static_analysis/lib/solidity.py
+++ code/static_analysis/lib/solidity.py
@@ -12,7 +12,7 @@ import sys
 import re
 import solcx
 import solidity_parser
-from version_parser import Version
+from version_parser import Version, VersionType
 from .printer import *
 
 # The mapping between solc versions installed by
@@ -25,7 +25,7 @@ class SolcVersionUnavailableException(Exception):
         self.solc_version = solc_version
     
     def __str__(self):
-        return 'solc %s not available' % self.solc_version
+        return f"Required solc version not installed, do `solc-select install {self.solc_version}`"
 
 
 class ParseException(Exception):
@@ -68,30 +68,6 @@ def parse_solc_version_string(solc_version_string):
 
 def get_available_solc_versions():
     global AVAILABLE_SOLC_VERSIONS
-
-    # solc versions installed by py-solc
-    py_solc_path = os.path.expanduser('~/.py-solc')
-    if os.path.isdir(py_solc_path):
-        py_solc_version_regex = os.path.join(py_solc_path, '*/bin/solc')
-        for solc_path in glob.glob(py_solc_version_regex):
-            solc_version = solc_path.split('/')[-3].replace('solc-', '')
-            AVAILABLE_SOLC_VERSION_PATHS[solc_version] = os.path.join(solc_path, 'solc')
-
-    # solc versions installed by py-solc-x
-    solcx_path = os.path.expanduser('~/.solcx')
-    if os.path.isdir(solcx_path):
-        py_solcx_version_regex = os.path.join(solcx_path, 'solc-v*')
-        for solc_path in glob.glob(py_solcx_version_regex):
-            solc_version = solc_path.split('/')[-1].replace('solc-', '')
-            AVAILABLE_SOLC_VERSION_PATHS[solc_version] = solc_path
-    
-    # solc versions installed by solc-select (shell script version)
-    solc_select_path = os.path.expanduser('~/.solc-select')
-    if os.path.isdir(solc_select_path):
-        solc_select_version_regex = os.path.join(solc_select_path, 'usr/bin/solc-v*')
-        for solc_path in glob.glob(solc_select_version_regex):
-            solc_version = solc_path.split('/')[-1].replace('solc-', '')
-            AVAILABLE_SOLC_VERSION_PATHS[solc_version] = solc_path
 
     # solc versions installed by solc-select (pip version)
     solc_select_path = os.path.expanduser('~/.solc-select/artifacts')
@@ -322,26 +298,25 @@ def get_solc_path(contract_path, log=None):
     # At this point, any solc version between maximum_min_version_string
     # and minimum_max_version_string should be able to compile this source.
     solc_version = str(maximum_min_version)
+    solc_version_no_v = str(maximum_min_version.get_typed_version(VersionType.STRIPPED_VERSION))
     if log:
         log.info('solc version: %s' % solc_version)
 
     # Get solc binary path that can compile this contract
-    solc_binary = 'solc-' + solc_version
+    solc_binary = 'solc-' + solc_version_no_v
 
     # Get installed solc versions
     installed_solc_versions = list(AVAILABLE_SOLC_VERSION_PATHS.keys())
 
     # Is the required solc version already installed?
     # Install otherwise
-    if solc_version not in installed_solc_versions:
-        try:
-            solcx.install_solc(solc_version)
-        except Exception as e:
-            print(e)
-            sys.exit(1)
+    print('solc_version_no_v: ', solc_version_no_v)
+    print(AVAILABLE_SOLC_VERSION_PATHS)
+    if solc_version_no_v not in installed_solc_versions:
+        raise SolcVersionUnavailableException(solc_version_no_v)
     
     # Form required the path pointing to the required solc binary
-    solc_path = AVAILABLE_SOLC_VERSION_PATHS[solc_version]
+    solc_path = os.path.join(AVAILABLE_SOLC_VERSION_PATHS[solc_version_no_v], solc_binary)
 
     return solc_version, solc_path
 
